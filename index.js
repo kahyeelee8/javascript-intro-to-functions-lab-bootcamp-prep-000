0873c3f06fd2e24b7cb794ccc46da15a0910cb59
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-07-28 02:28:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function logShout(string) { console.log("HELLO") }
 function logWhisper(string) {console.log("hello")}
 function sayHiToGrandma(string) { 
 if (string==="hello") {return "I can't hear you!"}
-else if (string==="I love you, Grandma.") {return "I love you,too."}
-else {return "YES, INDEED!"}
+else if (string==="I love you, Grandma.") {return "I love you, too."}
+else {return "YES INDEED!"}
 }
 
 
