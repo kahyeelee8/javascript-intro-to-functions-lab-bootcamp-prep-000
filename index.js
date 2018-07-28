72bfc77a885a13cecf32cacf3f749b632ecb2106
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-07-28 02:24:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function logShout(string) { console.log("HELLO") }
 function logWhisper(string) {console.log("hello")}
 function sayHiToGrandma(string) { 
 if (string==="hello") {return "I can't hear you!"}
-else if (string==="")
+else if (string==="I love you, Grandma.") {return "I love you "
 
 
 
